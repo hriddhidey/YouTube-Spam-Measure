249b6c317e5735a34052865d1032b7fc064f1c37
Hriddhi Dey <hriddhidey@gmail.com>
2016-04-25 23:10:53 +0530

    Specific video referncing done. Comment fetching failed since API deprecated.

--- crawler.py
+++ crawler.py
@@ -3,18 +3,23 @@ from apiclient.errors import HttpError
 from oauth2client.tools import argparser
 import matplotlib as plt
 import pandas as pd
+import gdata.youtube
+import gdata.youtube.service
 
 DEVELOPER_KEY = "AIzaSyAYspYcWY0lUmuUTJdn4YEsJA7SuttyZz8" 
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
-argparser.add_argument("--q", help="Search term", default="")
+#UZW2hs-2OAI
+var = "UZW2hs-2OAI"
+
+argparser.add_argument("--q", help="Search term", default=var)
 argparser.add_argument("--max-results", help="Max results", default=10)
 args = argparser.parse_args()
 options = args
 
 youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
-
+yt_service = gdata.youtube.service.YouTubeService()
 
 search_response = youtube.search().list(
     q=options.q,
@@ -43,18 +48,25 @@ for i in videos_list_response['items']:
     temp_res.update(i['statistics'])
     res.append(temp_res)
 
+#for row in res:
+#    print row,'\n'
 #df = pd.DataFrame.from_dict(res)
 #print df.head(10),'\n'
 #print df.describe(),'\n'
 #print df['likeCount'].mean()
 #print df
 
+#comment_feed = yt_service.GetYouTubeVideoCommentFeed(video_id=var)
+
+#for comment_entry in comment_feed.entry:
+ # print comment_entry.ToString()
+
 
 f = open("data.txt","w")
 headers = " ".ljust(3)+"Title".ljust(100)+"Views".ljust(15)+"Likes".ljust(15)+"Dislikes".ljust(15)+"Comments".ljust(15)
 f.write(headers)
 f.write('\n')
-for i in range(len(res)-1):
+for i in range(len(res)):
     entry = str(i+1).ljust(3)
     name = str(res[i]['v_title'].split()).encode('ascii').replace("u'","").replace("'","")
     name = ''.join(name).replace(",","")
